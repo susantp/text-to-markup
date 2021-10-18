69817f598a1195f87deab36452d68ab04e85e4a8
Susant Paudel <techbizznepal@gmail.com>
2021-10-18 20:37:17 +0545

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { CKEditor } from "ckeditor4-react";
 
 export default function Home() {
+  const ckEditorConfig = {
+    width: "80vw",
+    height: "47vh",
+  };
+  // const ckEditorStyle = {};
+  const message = "Past here";
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +19,29 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>Paste Here</h1>
+        <CKEditor
+          config={ckEditorConfig}
+          // style={ckEditorStyle}
+          // initData={<p>Hello from CKEditor 4!</p>}
+          onInstanceReady={() => {
+            // console.log("Editor is ready!");
+          }}
+        />
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://susantpaudel.com.np"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/logo.png" alt="Techbizz Logo" width={80} height={80} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
